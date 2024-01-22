ca019809ab7ef1f5e41ab22750f664f65c7d8dcf
ybh <bh78852@naver.com>
2024-01-22 17:53:07 +0900

    navigate #1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import SignUp from './components/SignUp';
+import {Route, Routes } from 'react-router-dom';
+import SignUp from './components/pages/SignUp';
+import Login from './components/pages/Login';
+import Mypage from './components/pages/Mypage';
+import Cart from './components/pages/Cart';
+import CustomerService from './components/pages/CustomerService';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <Routes>
-      <Route path='signUp' element={<SignUp/> } />
-    </Routes>
+      <Routes>
+        <Route element={<Layout />} >
+          <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/mypage' element={<Mypage />}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/customer-service' element={<CustomerService/>}/>
+        </Route>
+      </Routes>
   );
 }
 
